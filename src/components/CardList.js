193ee06f2697c0af2164f5f1cb4d78f3306fd70e
ayushgpt01 <ayushgpt043@gmail.com>
2023-08-18 20:51:06 +0530

    fix

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,20 +1,14 @@
 import React from "react";
-import Card from './Card';
+import Card from "./Card";
 
-const CardList = ({robots}) => {
-    const CardArray = robots.map((user,i) => {
-        return <Card 
-        key={robots[i].id} 
-        id={robots[i].id} 
-        name={robots[i].name} 
-        email={robots[i].email}
-        />
-    })
-    return (
+const CardList = ({ robots }) => {
+  return (
     <div>
-        {CardArray}
+      {robots.map((user) => (
+        <Card key={user.id} id={user.id} name={user.name} email={user.email} />
+      ))}
     </div>
-    );
-}
+  );
+};
 
 export default CardList;
